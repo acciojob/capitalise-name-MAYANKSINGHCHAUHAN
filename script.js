1bbcf5b21e99787b30c908b8d1155394bc25d43b
Mayank singh chauhan <113712051+MAYANKSINGHCHAUHAN@users.noreply.github.com>
2025-02-02 16:31:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+// Function to capitalize input value when the input field loses focus
+document.getElementById('fname').addEventListener('blur', function() {
+    // Get the current value of the input field
+    let inputValue = this.value;
+    
+    // Convert the value to uppercase
+    this.value = inputValue.toUpperCase();
+});
